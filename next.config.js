f15490d436088a4844f8bc74c296cd6bc2fe7132
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2021-10-29 10:24:59 +0200

    feat: init + index

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require('next-pwa/cache')
-const path = require('path')
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
     runtimeCaching,
   },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+  
 });
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import styles from '../styles/index.module.scss'
+import Button from '../components/Button/Button'
+
 export default function Home() {
   return (
-    <div /*className={styles.container}*/>
-      Hello Word
-    </div>
+      <div className={styles.container}>
+        <div>
+          <h1>My strange Collection</h1>
+          <p>My Strange Collection est une web application qui vous permet d’inventorier votre collection de comics.
+            <br/>
+            Vous pourrez indiquer l’état de vos exemplaires et en connaitre une estimation basée sur les données du site internet comicsvf.com en 2021.
+            </p>
+          <Button href="#" title="Se connecter"/>
+          <Button href="#" title="S'inscrire" color="white"/>
+        </div>
+      </div>
   )
 }
