4f0c31e3c18debe1ccf2d5030650cddc77efbf1b
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2021-12-20 11:20:03 +0100

    fix: favicon + logo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import styles from "../styles/index.module.scss";
 import Button from "../components/Buttons/Button";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <div>
+        <Image src="/icons/logo.svg" alt="" width={175} height={175} />
         <h1>My strange Collection</h1>
         <p>
           My Strange Collection est une web application qui vous permet
